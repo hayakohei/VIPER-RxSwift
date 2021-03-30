cb1cd4f58c7b7448aa803d8cdd7d5aa63895bc58
hayakohei <k.hayashi@amazia.co.jp>
2021-03-30 20:00:08 +0900

    chore: execute pod install

---
+++ Pods/Target Support Files/Pods-VIPER+RxSwift/Pods-VIPER+RxSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VIPER_RxSwift : NSObject
+@end
+@implementation PodsDummy_Pods_VIPER_RxSwift
+@end
---
+++ Pods/Target Support Files/Pods-VIPER+RxSwiftTests/Pods-VIPER+RxSwiftTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VIPER_RxSwiftTests : NSObject
+@end
+@implementation PodsDummy_Pods_VIPER_RxSwiftTests
+@end
