b30111ab1613430b613c56a3ec47c580fcd58892
hayakohei <k.hayashi@amazia.co.jp>
2021-03-30 20:01:49 +0900

    add: gitignore

--- Pods/Target Support Files/Pods-VIPER+RxSwift-VIPER+RxSwiftUITests/Pods-VIPER+RxSwift-VIPER+RxSwiftUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_VIPER_RxSwift_VIPER_RxSwiftUITests : NSObject
-@end
-@implementation PodsDummy_Pods_VIPER_RxSwift_VIPER_RxSwiftUITests
-@end
--- Pods/Target Support Files/Pods-VIPER+RxSwiftTests/Pods-VIPER+RxSwiftTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_VIPER_RxSwiftTests : NSObject
-@end
-@implementation PodsDummy_Pods_VIPER_RxSwiftTests
-@end
